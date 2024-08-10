24f633484ecb6841198c917022f28b0394e15875
fhcleriston <fhcleriston@gmail.com>
2024-08-10 13:53:05 -0300

    Iniciando a publicação

--- MoneyExtension/MoneyExtension.cs
+++ MoneyExtension/MoneyExtension.cs
@@ -4,9 +4,6 @@ public static class Money
 {
     public static int ToCents(this decimal valor)
     {
-        if (valor <= 0)        
-            return 0;
-
-        return decimal.ToInt32(valor * 100);
+         return decimal.ToInt32(valor * 100);
     }
 }
